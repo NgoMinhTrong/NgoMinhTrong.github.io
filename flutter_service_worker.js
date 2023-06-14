42273dee9f9cbfa2a26d0abf83156ce821792d2d
NgoMinhTrong <ngominhtrong53@gmail.com>
2023-06-15 03:20:55 +0700

    15.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "8c81d44a1c3c9b8ac29fdfe77ac69ef0",
-"index.html": "01946abed407790b97a83a96dc44b1d8",
-"/": "01946abed407790b97a83a96dc44b1d8",
-"main.dart.js": "1f3047c08ef000c5ae12f07059bb01b4",
+"index.html": "b7c137bbd425cb4d145267231b0c8301",
+"/": "b7c137bbd425cb4d145267231b0c8301",
+"main.dart.js": "84f77dfb21c355e9d8cda6033798c028",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "9161e869a1083833e100c7dd240bb607",
 "icons/Icon-192.png": "ca0f3d7b1a2fd6c75de1c74350b3720a",
